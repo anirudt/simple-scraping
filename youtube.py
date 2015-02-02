4be0e1d39578955f42921c01f809863bc9eaf7da
Anirud <anirudt@gmail.com>
2015-02-02 21:03:16 +0530

    added website which works

--- youtube.py
+++ youtube.py
@@ -7,7 +7,7 @@ def getLinks():
   parent_link = "https://www.youtube.com/watch?v=ZZ4cqPkdEPc&list=PL2D862035774C877C"
   relative_parent_link = "/watch?v=ZZ4cqPkdEPc&list=PL2D862035774C877C"
   hxs = lxml.html.document_fromstring(requests.get(parent_link).content)
-  links = hxs.xpath('//li[@class="yt-uix-scroller-scroll-unit "]/a/@href')#div[@class="playlist-videos-container yt-scrollbar-dark yt-scrollbar"]/text()')#/ol[@class="playlist-videos-list yt-uix-scroller"]/li[@class="yt-uix-scroller-scroll-unit "]
+  links = hxs.xpath('//li[@class="yt-uix-scroller-scroll-unit "]/a/@href')
   # div/a/@href gets some results.
   i = 0
   for e in links:
@@ -20,16 +20,13 @@ def getLinks():
   while(i<len(links)):
   	print links(i)
   	i = i + 1	
-  download_link = "http://www.youtube-mp3.org/"
-  #down = lxml.html.document_fromstring(requests.get(download_link).content)
-  #form = down.forms[0]
-  form_data = {'text':parent_link, 'submit':'submit'}
-#  result = parse(submit_form(page.forms[0])).getroot()
+  download_link = "http://www.vidtomp3.com/"
+  down = lxml.html.document_fromstring(requests.get(download_link).content)
+  down.forms[0].fields['url'] = download_link  
+  result = lxml.html.parse(lxml.html.submit_form(down.forms[0])).getroot()
 #  print form.inputs
 #  form.inputs.type['text'] = 'Anirud'
 #  print form.inputs.type['text']
-  r = requests.post(download_link, form_data)
-  print r.text  
-  
+  print result
 
 
