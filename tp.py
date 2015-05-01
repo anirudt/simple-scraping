8244ed44963cce4e512e344412c12415913b30cb
Anirud <anirudt@gmail.com>
2015-05-01 21:36:39 +0530

    Updated TnP

--- tp.py
+++ tp.py
@@ -7,12 +7,12 @@ import lxml.html
 
 
 def getLinks(w):
-  f = open('output.txt','w')
-  f.write(w+'\n')
+  f = open(w+'.txt','wb')
+  f.write(w.upper()+'\n\n')
   print "Getting links:\n"
   links = []
   text_links = []
-  for j in range (75,125):
+  for j in range (30,125):
     hxs = lxml.html.document_fromstring(requests.get("http://tp.iitkgp.ernet.in/notice/index.php?page=" + str(j)).content)
     try:
       l = hxs.xpath('//a[@class="notice"]/@href')
@@ -33,20 +33,77 @@ def getLinks(w):
     else:
       links_tmp.append('/' + e)
   #print text_links  
+  
   data = []
-  i = 0  
+  i = 0 
+  datastr = ""
+  maxi = 0
+  data1 = [] 
   for e in text_links:
     e = e.lower()
     if(e.find(w)!=-1):
-      print e
+      #print e
       hx = lxml.html.document_fromstring(requests.get("http://tp.iitkgp.ernet.in/notice"+links_tmp[i]).content)
       print links_tmp[i]
       f.write("http://tp.iitkgp.ernet.in/notice"+links_tmp[i]+'\n') 
       tmp = hx.xpath('//div/text()')
-      data.append(tmp) 
+      print tmp
+      f.write(u' '.join(tmp).encode('utf-8'))
+      f.write('\n')
+      data.append(tmp)
+      #print tmp	 
     i = i + 1 
-  print data
-  for e in data:
-    print e 
-    f.write("%s\n" % e)
+  f.close()
+
+def topLinks(w):
+  f = open(w+'.txt','wb')
+  f.write(w.upper()+'\n\n')
+  print "Getting links:\n"
+  links = []
+  text_links = []
+  for j in range (30,55):
+    hxs = lxml.html.document_fromstring(requests.get("http://tp.iitkgp.ernet.in/notice/index.php?page=" + str(j)).content)
+    try:
+      l = hxs.xpath('//a[@class="notice"]/@href')
+      g = hxs.xpath('//a[@class="notice"]/text()')
+      text_links = text_links + g
+      links = links + l
+    except IndexError:
+      print "error"
+    print j  
+#  print links
+  print "Check1"
+  links_tmp=[]
+# Link rectification 
+  for e in links:
+    if(e[0]=='/'):
+      links_tmp.append(e);
+      continue  
+    else:
+      links_tmp.append('/' + e)
+  #print text_links  
+  
+  data = []
+  i = 0 
+  datastr = ""
+  maxi = 0
+  data1 = [] 
+
+  for e in text_links:
+    e = e.lower()
+    #if(e.find(w)!=-1):
+      #print e
+    hx = lxml.html.document_fromstring(requests.get("http://tp.iitkgp.ernet.in/notice"+links_tmp[i]).content)
+    print links_tmp[i]
+    tmp = hx.xpath('//div/text()')
+    for f in tmp:
+      if(f.find(w)!=-1):
+        print "Company "+ str(i)
+        f.write("http://tp.iitkgp.ernet.in/notice"+links_tmp[i]+'\n') 
+        f.write(text_links[i]+'\n')
+        print tmp
+        f.write(u' '.join(tmp).encode('utf-8'))
+        f.write('\n\n')
+    #print tmp   
+    i = i + 1 
   f.close()
